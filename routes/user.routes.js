2f66a59df99016187810f2dd13c19945da67b01f
Adarsh33K <adarsh.sinha33@gmail.com>
2024-11-29 00:25:14 +0530

    TOKEM

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -30,7 +30,7 @@ router
   .get(authorizeRoles("admin", "manager", "user"), getUserProfile);
 router.route("/update").put(authorizeRoles("admin", "manager", "user"), update);
 router.route("/update/:user_id").put(authorizeRoles("admin"), updateByAdmin);
-router.route("/user-data/:user_id").get(authorizeRoles("admin"), getUser);
+router.route("/user-data/:user_id").get( getUser);
 
 router.route("/logout").get(logoutUser);
 
